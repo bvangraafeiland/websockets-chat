656aef5ad34bd435f1564c76480af14fa7e5a11e
Bastiaan van Graafeiland <bastiaan89@live.nl>
2017-01-14 16:34:33 +0100

    removed jquery, fixed package json for hot reloading

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -3,7 +3,7 @@
         <div class="panel-heading">Example Component</div>
 
         <div class="panel-body">
-            I'm  an example component and hmr maybe worked
+            I'm  an example component and hmr maybe worked? yay!
         </div>
     </div>
 </template>
