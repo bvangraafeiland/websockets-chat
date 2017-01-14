94972bf0620996d61e05d2e023dd35fa69b22f80
Bastiaan van Graafeiland <bastiaan89@live.nl>
2017-01-14 22:20:45 +0100

    dont require authentication for chat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,20 +3,9 @@
 namespace App\Http\Controllers;
 
 use Auth;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show the application dashboard.
      *
